9b661fcf3ff77984b5c97bf9989af9d5d7c76502
Sam Jacobson <s3491184@student.rmit.edu.au>
2018-09-24 18:14:36 +1000

    layout stuff

--- app/src/main/java/com/theheatseekers/brah/MainActivity.java
+++ app/src/main/java/com/theheatseekers/brah/MainActivity.java
@@ -2,12 +2,29 @@ package com.theheatseekers.brah;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button showFactButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Assign views from layout file to class variable
+        showFactButton = findViewById(R.id.sendBrah);
+
+        View.OnClickListener listener = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "I got here", Toast.LENGTH_LONG).show();
+
+            }
+        };
+        showFactButton.setOnClickListener(listener);
     }
 }
